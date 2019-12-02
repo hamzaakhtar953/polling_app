e69aea7ccc16df23885e71d37f2887177cc7b33b
hamzaakhtar <hamza.akhtar@arbisoft.com>
2019-12-02 19:59:30 +0500

    Modified admin panel for Question view

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    publication_date = models.DateTimeField('date_published')
+    publication_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
@@ -14,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.publication_date <= now
+    was_published_recently.admin_order_field = 'publication_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
